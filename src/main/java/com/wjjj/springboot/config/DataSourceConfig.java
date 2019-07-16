f79ddcf2664763d894d15471abbd68335d7d6242
65286 <244731110@qq.com>
2019-07-16 17:29:04 +0800

    添加双数据源

---
+++ src/main/java/com/wjjj/springboot/config/DataSourceConfig.java
@@ -0,0 +1,45 @@
+package com.wjjj.springboot.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by WJJJ on 2019/7/16.
+ */
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "oracleDataSource")
+    @Qualifier("oracleDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.oracle")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "mssqlDataSource")
+    @Qualifier("mssqlDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.mssql")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+
+    @Bean(name = "oracleJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(@Qualifier("oracleDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "mssqlJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(@Qualifier("mssqlDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
--- src/main/java/com/wjjj/springboot/quart/SchedulerTask.java
+++ src/main/java/com/wjjj/springboot/quart/SchedulerTask.java
@@ -1,11 +1,12 @@
 package com.wjjj.springboot.quart;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,21 +21,30 @@ import java.util.Map;
 @Component
 public class SchedulerTask {
 
-    @Resource
-    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    @Qualifier("oracleJdbcTemplate")
+    private JdbcTemplate oracleJdbcTemplate;
+
+    @Autowired
+    @Qualifier("mssqlJdbcTemplate")
+    private JdbcTemplate mssqlJdbcTemplate;
 
     private List<Map<String, String>> mapList = new ArrayList<>();
 
     @Scheduled(cron = "*/6 * * * * ?")
     private void process() {
 
-        String sql = "select * from t_dict";
-        List<Map<String, Object>> resultList = jdbcTemplate.queryForList(sql);
-
+        String oraclesql = "select * from WX_ALI_CONFIG";
+        List<Map<String, Object>> resultList = oracleJdbcTemplate.queryForList(oraclesql);
         //循环打印结果
         resultList.stream().forEach(System.out::println);
+        log.info("#########  oracle数据库查询  #########");
 
-        log.info("#########  info  #########");
+        String serversql = "select * from WXANDALIPAYCONFIG";
+        List<Map<String, Object>> mapList = mssqlJdbcTemplate.queryForList(serversql);
+        //循环打印结果
+        mapList.stream().forEach(System.out::println);
+        log.info("#########  server  #########");
 
     }
 }
